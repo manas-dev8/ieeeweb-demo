97a20a97e5ef55b332cd7612161e4dd6b4c78fc4
Manas Saxena <156520126+manas-dev8@users.noreply.github.com>
2025-02-04 16:32:13 +0000

    fixed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -30,8 +30,8 @@ export default function Footer() {
               <Image
                 src="https://res.cloudinary.com/anurag256k/image/upload/v1735541549/IEEE/galgotias_v0ccp4.png"
                 alt="Galgotias University Logo"
-                width={140}
-                height={100}
+                width={100}
+                height={90}
               />
             </div>
             <p className="text-sm text-gray-500">
